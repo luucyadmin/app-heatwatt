a42ea2ff4d84ca76c7b3996d44108721849b2a9f
Jakub Stejskal <jakubstejskal22@gmail.com>
2024-01-24 20:56:20 -0500

    LU-7064 crowdin integration

---
+++ i18n/fr.ts
@@ -0,0 +1,7 @@
+export default {
+  Volume: 'Volume',
+  Heating_Cost_Per_Cubic_Meter: 'Heating cost per month per m³',
+  Monthly_Cost: 'Monthly cost',
+  Yearly_Cost: 'Yearly cost',
+  Total_Cost_10_Years: '10 Years + {{maintenanceCost}}% Maintenance',
+};
